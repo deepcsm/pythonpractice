3e3ab3eb3c87d8b93fd237cb9b486b34a8265b24
deepcsm <deep.csm@gmail.com>
2017-03-14 10:23:51 +0530

    initail commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+# This Programs says hello
+
+print ('Hello World')
+print ('What is your name?')# ask for their name
+myname=input()
+print ('It is good to meet you ,'+myname)
+print ('The lenght of your name is :')
+print (len(myname))
+print ('what is your age ?:')
+myage=input()
+print ('you will be '+str(int(myage)+1) +'in a year')
+
---
+++ script1.py
@@ -0,0 +1,9 @@
+import sys
+def main():
+	a="Hello This is Python"
+	for i in a:
+		print (i)
+	sys.exit()
+
+if __name__=='__main__':
+	main()
